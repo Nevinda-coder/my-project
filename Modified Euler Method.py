5b4b012a84e9f1d41071fca3e6a2ff73f438d77b
Nevinda <nevinda.17@business.mrt.ac.lk>
2020-10-10 16:58:44 +0530

    Minor variable change in Modified Euler Method.py

--- Modified Euler Method.py
+++ Modified Euler Method.py
@@ -3,21 +3,21 @@ def func( x, y ):
 	return (3*(18-y))
  	
 # Function for euler formula 
-def euler( x0, y, h, x ): 
+def euler( x0, y, H, x ): 
 	# Iterating till the point at which we 
 	# need approximation 
 	while x0 < x: 
-		y= y +h*(func(x0+(h/2), y+(h/2)*func(x0,y)))
-		x0 = x0 + h 
+		y= y +H*(func(x0+(H/2), y+(H/2)*func(x0,y)))
+		x0 = x0 + H 
 	# Printing approximation 
-	print("Approximate solution at x = ", x, " is ", "%.6f"% y) 
+	print("Solution at x = ", x, " is ", "%.6f"% y) 
 	 
 # Initial Values 
 x0 = 0
 y0 = 14
-h = 0.2
+H = 0.2
 x = 1 # Value of x at which we need approximation 
-euler(x0, y0, h, x) 
+euler(x0, y0, H, x) 
 
 
 
